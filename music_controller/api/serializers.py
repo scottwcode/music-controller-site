eb4aaf87d874d7d8aa7425a75a461b715d67df99
scottwcode <scottmail77@gmail.com>
2020-11-30 19:00:56 -0500

    changes through React Router and Building Components and mods to ignore files

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -3,7 +3,7 @@ from .models import Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Room
-    fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
-
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -4,8 +4,8 @@ from .serializers import RoomSerializer
 from .models import Room
 
 # Create your views here.
-class RoomView(generics.ListAPIView):
-  queryset = Room.objects.all()
-  serializer_class = RoomSerializer
-  
 
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
